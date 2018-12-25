35a6ed50c8a789c3b0b75cd0f46d5f1e8fdc0ade
Student <>
2018-12-25 16:22:58 +0200

    initial commit

---
+++ Square/Square/SquareView.swift
@@ -0,0 +1,13 @@
+//
+//  SquareView.swift
+//  Square
+//
+//  Created by Mosin Dmitry on 25.12.2018.
+//  Copyright © 2018 IDAP. All rights reserved.
+//
+
+import UIKit
+
+class SquareView: UIView {
+
+}
---
+++ Square/Square/SquareViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SquareViewController.swift
+//  Square
+//
+//  Created by Mosin Dmitry on 25.12.2018.
+//  Copyright © 2018 IDAP. All rights reserved.
+//
+
+import UIKit
+
+class SquareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
