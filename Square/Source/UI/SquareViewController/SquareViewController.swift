cb1d405650aca8d44764673340534f6ca019984a
Student <>
2018-12-26 15:57:18 +0200

    Fixed project struct

--- Square/Source/UI/SquareViewController/SquareViewController.swift
+++ Square/Source/UI/SquareViewController/SquareViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class SquareViewController: UIViewController {
 
     var squareView: SquareView? {
-
         if self.isViewLoaded {
             return self.view as? SquareView
         }
