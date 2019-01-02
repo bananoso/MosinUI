083f55da0ad1d680a9b9786986e306f6d945c74e
Student <>
2019-01-02 12:56:41 +0200

    Add CGRect corner points

--- Square/Source/Lib/Extensions/UIKit/CGRect+Extensions.swift
+++ Square/Source/Lib/Extensions/UIKit/CGRect+Extensions.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2018 IDAP. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension CGRect {
+    
+    var topLeft: CGPoint {
+        return CGPoint(x: self.minX, y: self.minY)
+    }
+    
+    var topRight: CGPoint {
+        return CGPoint(x: self.maxX, y: self.minY)
+    }
+    
+    var bottomRight: CGPoint {
+        return CGPoint(x: self.maxX, y: self.maxY)
+    }
+    
+    var bottomLeft: CGPoint {
+        return CGPoint(x: self.minX, y: self.maxY)
+    }
+    
+    var leftCenter: CGPoint {
+        return CGPoint(x: self.minX, y: self.midY)
+    }
+    
+    var rightCenter: CGPoint {
+        return CGPoint(x: self.maxX, y: self.midY)
+    }
+    
+    var center: CGPoint {
+        return CGPoint(x: self.midX, y: self.midY)
+    }
+    
+    var topCenter: CGPoint {
+        return CGPoint(x: self.midX, y: self.minY)
+    }
+    
+    var bottomCenter: CGPoint {
+        return CGPoint(x: self.midX, y: self.maxY)
+    }
+}
